5840932ab01602daad075406ec34941ee9f544ea
Steve <steven.mcilwaine@costaworld.com>
2020-09-07 13:36:52 +0100

    Added countdown script to roughly emulate a countdown timer setup - very rough as not sure what the original on the site is using, but close enough as an example of vanilla JS

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./img/*.svg";
 import "./img/*.jpg";
 
 import "./js/wines.json";
+import "./js/*.js";
 
 import Vue from 'vue';
 import App from './App';
---
+++ src/js/countdown.js
@@ -0,0 +1,57 @@
+const countdown = document.querySelectorAll('[data-js-countdown]');
+
+function leadingZeroesString(number) {
+  let n = number.toString();
+  if (n.length < 2) { n = `0${n}`; }
+  return n;
+}
+
+function countdownTimer(element) {
+  const timeNow = new Date();
+  let cutoff = new Date(`${timeNow.getMonth() + 1} ${timeNow.getDate()}, ${timeNow.getFullYear()} 17:00:00`);
+  let timeUntilDeliveryCutoff;
+  
+  if (timeNow.getDay() === 0) {
+    // if sunday, change to monday delivery deadline
+    cutoff.setDate(cutoff.getDate() + 1);
+  } else if (timeNow.getDay() === 6 && timeNow.getHours() >= 17) {
+    // if saturday past 5, change to monday delivery deadline
+    cutoff.setDate(cutoff.getDate() + 2);
+  }
+  
+  // Milliseconds until next cutoff
+  timeUntilDeliveryCutoff = Math.abs(cutoff - timeNow);
+
+  // Delivery day calculating (3 days later, with added day if lands on Sunday)
+  let deliveryDay = new Date(cutoff);
+  deliveryDay.setDate(deliveryDay.getDate() + 3);
+  if (deliveryDay.getDate() === 0) { deliveryDay.setDate(deliveryDay.getDate() + 1); }
+
+  // Update timer if it is in DOM
+  if (element.querySelector('[data-js-countdown-timer]')) { 
+    const seconds = leadingZeroesString(Math.floor((timeUntilDeliveryCutoff / 1000) % 60));
+    const minutes = leadingZeroesString(Math.floor((timeUntilDeliveryCutoff / (1000 * 60)) % 60));
+    const hours = leadingZeroesString(Math.floor((timeUntilDeliveryCutoff / (1000 * 60 * 60)) % 24));
+
+    element.querySelector('[data-js-countdown-timer]').innerHTML = `${hours}:${minutes}:${seconds}`;
+  }
+
+  // Update delivery day if it is in DOM
+  if (element.querySelector('[data-js-countdown-delivery-day]') 
+    && element.querySelector('[data-js-countdown-delivery-day]').innerHTML !== ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][deliveryDay.getDay()]) {
+    element.querySelector('[data-js-countdown-delivery-day]').innerHTML = `${["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][deliveryDay.getDay()]}`;
+  }
+
+}
+
+if (countdown.length > 0) {
+
+  [...countdown].forEach(element => {
+    countdownTimer(element);
+
+    let interval = setInterval(() => {
+      countdownTimer(element);
+    }, 1000);
+  });
+
+}
